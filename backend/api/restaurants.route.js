145dce8097c2656bfad3458c9a9ffa114e4ed7d1
atlas25git <atlas.sarthak@gmail.com>
2021-05-11 18:18:01 +0530

    Serverless

---
+++ backend/api/restaurants.route.js
@@ -0,0 +1,21 @@
+import express from "express"
+import RestaurantsCtrl from "./restaurants.controller.js"
+import ReviewsCtrl from "./reviews.controller.js"
+
+
+const router = express.Router()
+
+router.route("/").get(RestaurantsCtrl.apiGetRestaurants)
+router.route("/id/:id").get(RestaurantsCtrl.apiGetRestaurantById)
+router.route("/cuisines").get(RestaurantsCtrl.apiGetRestaurantCuisines)
+
+
+//adding routes for users to post edit delete reviews:
+
+router
+    .route("/review")
+    .post(ReviewsCtrl.apiPostReview)
+    .put(ReviewsCtrl.apiUpdateReview)
+    .delete(ReviewsCtrl.apiDeleteReview)
+
+export default router
